af1e3d480588ae8d6e707ef1ff3baac55c081d77
allan Maubert <a.maubert.jouvet@gmail.com>
2020-03-24 08:37:08 +0100

    init commit

---
+++ kebab-shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kebab_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
